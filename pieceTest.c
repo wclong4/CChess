12e2da0c981fd4cbe4a8060bd6835aa15a6944da
Warren Long <wclong4@ncsu.edu>
2024-06-06 19:25:47 -0400

    Created a working board

---
+++ pieceTest.c
@@ -0,0 +1,14 @@
+// simple test program to see if piece works correctly
+#include <assert.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "piece.h"
+
+int main()
+{
+  Piece board[8][8];
+  
+}
+
